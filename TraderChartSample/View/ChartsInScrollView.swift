8a6b6c2addb0ec85a4961ccc3dab79b51eb107ee
山本敬太 <yamamoto@altonotes.co.jp>
2025-09-24 06:48:37 +0900

    TurningPoint実装中

--- TraderChartSample/View/ChartsInScrollView.swift
+++ TraderChartSample/View/ChartsInScrollView.swift
@@ -5,30 +5,47 @@ struct ChartsInScrollView: View {
     
     @State var chartViews: [TraderChartView] = (0..<3).map { _ in TraderChartView() }
 
+    @State var turningPointsList: [[TurningPointCalculator.PointData]?] = []
+
     var chartData: [ChartData] = (0..<3).map { _ in ChartData.dummy() }
 
     init() {
         chartViews.enumerated().forEach { offset, chartView in
             chartView.setData(chartData[offset])
             initChart(chartView: chartView)
+            
+//            chartView.xAxis.indexOf(point: <#T##CGPoint#>, scrollOffset: chartView.scrollOffset)
+        }
+        turningPointsList = chartViews.map {
+            $0.turningPoint.results?.points
         }
     }
 
     var body: some View {
-        ScrollView {
+        VStack {
             ForEach(Array(chartViews.enumerated()), id: \.offset) { index, chartView in
                 TraderChartUI(chartView: chartView)
-                    .frame(height: 200)
+                    .frame(maxHeight: .infinity)
             }
         }
     }
-    
+
+    func test(index: Int) {
+        let points = turningPointsList.indices.contains(index) ? turningPointsList[index] : nil
+        guard let points else { return }
+        
+        points.map { point in
+            let chartView = chartViews[index]
+            point.index
+            chartView.xAxis.indexOf(point: $0, scrollOffset: chartView.scrollOffset)
+        }
+    }
+
     func initChart(chartView: TraderChartView) {
         var colorConfig = ColorConfig()
         colorConfig["background"] = UIColor.black
         chartView.colorConfig = colorConfig
         chartView.mainGraphArea.yAxis.designatedDecimalLength = 3
-        chartView.mainGraphArea.yAxis.isTouchEnabled = false
     }
 }
 
